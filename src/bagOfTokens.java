4bab510ee407b92733126e79765fe58827b7f7e0
tarunkumar6468 <tarun.22scse1010988@galgotiasuniversity.edu.in>
2024-03-05 22:47:57 +0530

    first commit by tarun

---
+++ src/bagOfTokens.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bagOfTokens {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int tokens[] = new int[n];
+        int power = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            tokens[i] = sc.nextInt();
+        }
+        int ans  = bagOfTokensScore(tokens,n,power);
+        System.out.println(ans);
+    }
+    public static int bagOfTokensScore(int arr[] , int n, int p){
+        int count = 0;
+        if(arr[0]>p)return 0;
+        Arrays.sort(arr);
+        for(int i = 0; i<n;i++){
+            if(p<=arr[i]){
+                p-=arr[i];
+
+                count++;
+            }
+            else if(count==1){
+                p = p + arr[n-1];
+                count--;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/minimumLengthOfStringAfterDeletingSimilarEnds.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class minimumLengthOfStringAfterDeletingSimilarEnds {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(); // aabcaabba
+
+        returnBalanceString(s); // answer = 3
+    }
+    public static void returnBalanceString(String s){
+        int l = s.length();
+        int left = 0;
+        int right = l-1;
+        while(left<right && s.charAt(left) == s.charAt(right)){
+            int temp = s.charAt(left);
+            while (left<=right && s.charAt(left)==temp){
+                left++;
+            }
+            while(left<=right && s.charAt(right) == temp){
+                right--;;
+            }
+        }
+        System.out.print(right-left+1);
+    }
+}
