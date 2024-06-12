8198e7f9b0032d4ce0c179905d30dc194da2d1fa
tarunkumar6468 <tarun.22scse1010988@galgotiasuniversity.edu.in>
2024-06-12 18:12:50 +0530

    sort_colors_update

---
+++ src/Days_30_sort_colors.java
@@ -0,0 +1,32 @@
+public class Days_30_sort_colors {
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        // Output: 0 0 1 1 2 2
+    }
+}
